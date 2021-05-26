2dcaa4973587e2d538d9644457ac3f0d2fe30b54
Kanapka1453 <48421395+Kanapka1453@users.noreply.github.com>
2021-05-26 20:17:07 +0200

    added fully working loadU1() method, which takes the ArrayList of Items returned from loadItems and starts creating U1 rockets

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Simulation newSimulation = new Simulation();
         newSimulation.loadItems();
+        newSimulation.loadU1(newSimulation.itemArrayList);
+
 
     }
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 
 public class Simulation {
 
+    int entirePayloadWeight;
+    ArrayList<Item> itemArrayList = new ArrayList<>();
+
     public ArrayList<Item> loadItems() {
-        ArrayList<Item> itemArrayList = new ArrayList<>();
         File phaseOneItems = new File("phase-1.txt");
         try {
             Scanner scanner = new Scanner(phaseOneItems);
@@ -32,9 +34,42 @@ public class Simulation {
 
     public ArrayList<U1> loadU1(ArrayList<Item> itemsToTakeArrayList) {
         ArrayList<U1> u1ArrayList = new ArrayList<>();
-
+        int payloadWeightLeft;
+        //wylicza wage wszystkich przedmiotow do zaladowania
+        payloadWeightLeft = sumPayloadWeight(itemsToTakeArrayList);
+        System.out.println("Total weight:" + payloadWeightLeft);
+        u1ArrayList.add(0,new U1());
+        //gdy aktualna waga danej rakiety U1 jest <= maksymalnemu udzwigowi i waga pozostałych do załadowania przedmiotów !=0
+        int i=0; // while loop rocket counter
+        int j=0; // while loop items counter
+        while ((u1ArrayList.get(i).rocketCurrentWeight<=u1ArrayList.get(i).rocketMaxWeightWithCargo)&&payloadWeightLeft!=0) {
+            if(u1ArrayList.get(i).rocketCurrentWeight+itemsToTakeArrayList.get(j).getWeight()<=u1ArrayList.get(0).rocketMaxWeightWithCargo){
+                u1ArrayList.get(i).rocketCurrentWeight+=itemsToTakeArrayList.get(j).getWeight();
+                payloadWeightLeft=payloadWeightLeft-itemsToTakeArrayList.get(j).getWeight();
+                j++;
+                System.out.println("DEBUG:waga rakiety "+i+" wynosi "+u1ArrayList.get(i).rocketCurrentWeight);
+            }else {
+                u1ArrayList.add(new U1());
+                i++;
+                System.out.println("DEBUG:stworzono rakietę: "+i);
+            }
+            System.out.println("DEBUG:payload left: "+payloadWeightLeft);
+            System.out.println("DEBUG:załadowana została rakieta: "+i);
+        }
 
         return u1ArrayList;
     }
+
+    private int sumPayloadWeight(ArrayList<Item> itemsToTakeArrayList) {
+         /*for (int i = 0; i < itemsToTakeArrayList.size(); i++) {
+            entirePayloadWeight += itemsToTakeArrayList.get(i).getWeight();
+        }*/
+        //enhaced for - for each loop
+        for (Item item : itemsToTakeArrayList) {
+            entirePayloadWeight += item.getWeight();
+        }
+        return entirePayloadWeight;
+    }
+
 
 }
