0242cc66de4623b5235efe842936219ab0ac5cf5
Kanapka1453 <48421395+Kanapka1453@users.noreply.github.com>
2021-05-27 22:08:07 +0200

    fixed launch() name method, now both launch and land methods are working properly

--- src/U1.java
+++ src/U1.java
@@ -1,11 +1,11 @@
 public class U1 extends Rocket {
+    int currentPayloadWeightU1 = rocketCurrentWeight - rocketWeight;
 
     @Override
     // dlaczego tutaj nie moze byc np private, sprawdzic czy mozna zredukowac widocznosc przy override
-    public boolean land() {
-        int currentPayloadWeightU1 = rocketCurrentWeight - rocketWeight;
+    public boolean launch() {
         //cast do double niezbedny zeby nie ucielo wartosci po przecinku
-        double crashChanceU1 = 5 * ((double) currentPayloadWeightU1 / rocketCargoLimit);
+        double crashLaunchChanceU1 = 5 * ((double) currentPayloadWeightU1 / rocketCargoLimit);
         /*
         if (Math.random()*100 < crashChanceU1) {
             return false;
@@ -15,7 +15,13 @@ public class U1 extends Rocket {
         */
 
         //simplified version
-        return !(Math.random() * 100 < crashChanceU1);
+        return !(Math.random() * 100 < crashLaunchChanceU1);
+    }
+
+    @Override
+    public boolean land() {
+        double crashLandingChanceU1 = 1 * ((double) currentPayloadWeightU1 / rocketCargoLimit);
+        return !(Math.random() * 100 < crashLandingChanceU1);
     }
 
 
--- src/U2.java
+++ src/U2.java
@@ -1,12 +1,17 @@
 public class U2 extends Rocket {
+    int currentPayloadWeightU2 = rocketCurrentWeight - rocketWeight;
+
+    @Override
+    public boolean launch() {
+        double crashLaunchChanceU2 = 4 * ((double) currentPayloadWeightU2 / rocketCargoLimit);
+        return !(Math.random() * 100 < crashLaunchChanceU2);
+    }
 
     @Override
     public boolean land() {
-        int currentPayloadWeightU2 = rocketCurrentWeight - rocketWeight;
-        double crashChanceU2 = 4 * ((double) currentPayloadWeightU2 / rocketCargoLimit);
-        return !(Math.random() * 100 < crashChanceU2);
+        double crashLandingChanceU2 = 8 * ((double) currentPayloadWeightU2 / rocketCargoLimit);
+        return !(Math.random() * 100 < crashLandingChanceU2);
     }
-
 
     //constructor
     public U2() {
